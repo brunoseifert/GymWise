0e51d1529a8531e8dcd9c0af5d41db445f46ef71
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-06-07 19:23:38 -0300

    feat: Page Dashboard init

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ExercisePage from "./pages/Exercise/Exercise";
 import RatingPage from "./pages/Rating/Rating";
+import DashboardPage from "./pages/Dashboard/page";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/exercises" element={<ExercisePage />} />
         <Route path="/rating" element={<RatingPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Dashboard/page.tsx
@@ -0,0 +1,105 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+import { MenuIcon, LogInIcon, Dumbbell, Bolt, User2Icon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { DataTableDemo } from "./components/DataTable";
+
+const DashboardPage = () => {
+  return (
+    <div className=" flex flex-col">
+      <div className="flex py-4 justify-between px-4">
+        <div className="flex space-x-4 text-white">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div className="flex-col ">
+            <h1>Treinador Bruno</h1>
+            <span className="opacity-50">bruno@gmail.com</span>
+          </div>
+        </div>
+        <div>
+          {" "}
+          <Sheet>
+            <SheetTrigger>
+              <Button size="icon" variant="outline">
+                <MenuIcon className="text-white" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="right">
+              <SheetHeader className="text-left text-lg font-semibold text-purple-100 p-6">
+                Menu
+              </SheetHeader>
+              <Separator className=" w-full opacity-15" />
+              <div className="flex flex-col  text-white p-6">
+                <SheetClose>
+                  <Button
+                    size="icon"
+                    variant="outline"
+                    className="w-full justify-start gap-2 bg-[#26272B] px-2"
+                  >
+                    <LogInIcon className="" />
+                    Login
+                  </Button>
+                </SheetClose>
+                <SheetClose>
+                  <Button
+                    size="icon"
+                    variant="outline"
+                    className="w-full justify-start gap-2 px-2"
+                  >
+                    <User2Icon className="" />
+                    Alunos
+                  </Button>
+                </SheetClose>
+                <SheetClose>
+                  <Button
+                    size="icon"
+                    variant="outline"
+                    className="w-full justify-start gap-2 px-2"
+                  >
+                    <Dumbbell className="" />
+                    Exercícios
+                  </Button>
+                </SheetClose>
+
+                <SheetClose>
+                  <Button
+                    size="icon"
+                    variant="outline"
+                    className="w-full justify-start gap-2 px-2"
+                  >
+                    <Bolt className="" />
+                    Configurações
+                  </Button>
+                </SheetClose>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+      <Separator className=" w-full opacity-15" />
+      <div className="flex p-4">
+        <span className="text-primaryPurple text-3xl ">Dashboard</span>
+        <Button
+          variant="outline"
+          className="bg-primaryPurple text-white p-2 pl-4 pr-4 rounded-xl ml-auto"
+        >
+          Cadastrar
+        </Button>
+      </div>
+      <div className="px-4 ">
+        <DataTableDemo />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
