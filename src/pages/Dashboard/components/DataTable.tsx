fcb1abf184698eb0e0949482711b947bd81ae928
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-06-14 19:17:25 -0300

    feat: Form Register Student Function

--- src/pages/Dashboard/components/DataTable.tsx
+++ src/pages/Dashboard/components/DataTable.tsx
@@ -1,30 +1,14 @@
-"use client";
-
 import * as React from "react";
-import {
-  ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
+
 import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
 import {
   DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
+  DropdownMenuContent,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import {
@@ -45,129 +29,22 @@ export type Student = {
   dateOfBirth: string;
 };
 
-// eslint-disable-next-line react-refresh/only-export-components
-export const columns: ColumnDef<Student>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-        className="border-grayTwo"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className="border-white"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "status",
-    header: "Status",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("status")}</div>
-    ),
-  },
-  {
-    accessorKey: "Nome Completo",
-    header: "Nome Completo",
-    cell: ({ row }) => (
-      <div>{`${row.original.firstName} ${row.original.lastName}`}</div>
-    ),
-  },
-  {
-    accessorKey: "email",
-    header: ({ column }) => (
-      <Button
-        variant="ghost"
-        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-      >
-        Email
-        <ArrowUpDown className="ml-2 h-4 w-4" />
-      </Button>
-    ),
-    cell: ({ row }) => <div>{row.original.email}</div>,
-  },
-  {
-    accessorKey: "Celular",
-    header: () => <div className="text-right">Celular</div>,
-    cell: ({ row }) => (
-      <div className="text-right">{row.getValue("cellPhone")}</div>
-    ),
-  },
-  {
-    accessorKey: "Aniversário",
-    header: "Aniversário",
-    cell: ({ row }) => {
-      const date = new Date(row.getValue("dateOfBirth"));
-      return <div>{date.toLocaleDateString()}</div>;
-    },
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const payment = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
-
 export function DataTableDemo() {
   const [data, setData] = React.useState<Student[]>([]);
-  const [loading, setLoading] = React.useState(true);
+  const [loading, setLoading] = React.useState<boolean>(true);
   const [error, setError] = React.useState<string | null>(null);
-  const [sorting, setSorting] = React.useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    []
-  );
-  const [columnVisibility, setColumnVisibility] =
-    React.useState<VisibilityState>({});
-  const [rowSelection, setRowSelection] = React.useState({});
 
   React.useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch("http://localhost:5099/api/v1/students");
         if (!response.ok) {
-          throw new Error("Erro ao buscar os dados da API");
+          throw new Error("Failed to fetch data");
         }
         const data = await response.json();
-        setData(data);
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } catch (error: any) {
-        setError(error.message);
+        setData(data.items); // Assuming 'items' is the correct format from API
+      } catch (error) {
+        setError(error instanceof Error ? error.message : String(error));
       } finally {
         setLoading(false);
       }
@@ -176,145 +53,95 @@ export function DataTableDemo() {
     fetchData();
   }, []);
 
-  const table = useReactTable({
-    data,
-    columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-      rowSelection,
-    },
-  });
-
   if (loading) {
-    return <div>Carregando...</div>;
+    return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>{error}</div>;
+    return <div>Error: {error}</div>;
+  }
+
+  if (!Array.isArray(data)) {
+    return <div>No data available</div>;
   }
 
   return (
     <div className="w-full bg-transparent text-white">
       <div className="flex items-center py-4">
         <Input
-          placeholder="Buscar por nome..."
-          value={
-            (table.getColumn("Nome Completo")?.getFilterValue() as string) ?? ""
-          }
-          onChange={(event) =>
-            table.getColumn("Nome Completo")?.setFilterValue(event.target.value)
-          }
+          placeholder="Search by name..."
           className="max-w-sm text-grayOne"
+          // Placeholder for search functionality
         />
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" className="ml-auto">
-              Filtros <ChevronDown className="ml-2 h-4 w-4" />
+              Filters <ChevronDown className="ml-2 h-4 w-4" />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                );
-              })}
+            {/* Placeholder for filter options */}
+            <DropdownMenuLabel>Filter Options</DropdownMenuLabel>
+            <DropdownMenuItem>Option 1</DropdownMenuItem>
+            <DropdownMenuItem>Option 2</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
       <div className="rounded-md border border-white/10">
         <Table>
           <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
+            <TableRow>
+              <TableHead>Status</TableHead>
+              <TableHead>Nome Completo</TableHead>
+              <TableHead>
+                <Button variant="ghost">
+                  Email
+                  <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+              </TableHead>
+              <TableHead className="text-right">Celular</TableHead>
+              <TableHead>Aniversário</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
           </TableHeader>
           <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                  className="hover:bg-grayOne/10 transition-colors data-[state=checked]:bg-grayOne/10"
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id} className="backdrop-filter-none">
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  Sem resultados
+            {data.map((student) => (
+              <TableRow key={student.id}>
+                <TableCell>{student.firstName}</TableCell>
+                <TableCell>{student.lastName}</TableCell>
+                <TableCell>{student.email}</TableCell>
+                <TableCell className="text-right">
+                  {student.cellPhone}
+                </TableCell>
+                <TableCell>{student.dateOfBirth}</TableCell>
+                <TableCell>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="outline" className="h-8 w-8 p-0">
+                        <span className="sr-only">Open menu</span>
+                        <MoreHorizontal className="h-4 w-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                      <DropdownMenuItem>Copy Student ID</DropdownMenuItem>
+                      <DropdownMenuItem>View Details</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </TableCell>
               </TableRow>
-            )}
+            ))}
           </TableBody>
         </Table>
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} de{" "}
-          {table.getFilteredRowModel().rows.length} Alunos Selecionados.
-        </div>
+        <div className="text-sm text-gray-400">{data.length} Students</div>
         <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Anterior
+          <Button variant="outline" size="sm">
+            Previous
           </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Próximo
+          <Button variant="outline" size="sm">
+            Next
           </Button>
         </div>
       </div>
--- src/pages/Dashboard/register/page.tsx
+++ src/pages/Dashboard/register/page.tsx
@@ -1,12 +1,61 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 const RegisterStudents = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    phoneNumber: "",
+    dateOfBirth: "",
+    document: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const adjustedFormData = {
+      ...formData,
+      dateOfBirth: new Date(formData.dateOfBirth).toISOString(),
+    };
+
+    console.log(adjustedFormData);
+    try {
+      const response = await fetch(
+        "http://localhost:5099/api/v1/students/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(adjustedFormData),
+        }
+      );
+
+      const result = await response.json();
+      console.log(result);
+
+      if (!response.ok) {
+        throw new Error("Falha ao registrar aluno");
+      }
+      console.log("Aluno registrado com sucesso");
+    } catch (error) {
+      console.error("Erro ao registrar aluno:", error);
+    }
+  };
+
   return (
-    <div className="py-48 md:py-0  h-[800px] flex-col items-center justify-center md:grid lg:px-0">
+    <div className="py-52 md:py-0 h-[800px] flex-col items-center justify-center md:grid lg:px-0">
       <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[550px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight text-primaryPurple">
               Cadastrar Aluno
@@ -16,30 +65,39 @@ const RegisterStudents = () => {
             </p>
           </div>
           <div className="m-10 grid gap-6">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="grid gap-2">
                 <div className="flex gap-1">
                   <Label className="sr-only">firstName</Label>
                   <Input
+                    name="firstName"
                     placeholder="Nome"
                     type="text"
                     autoCapitalize="none"
                     autoCorrect="off"
+                    onChange={handleChange}
+                    value={formData.firstName}
                   />
                   <Label className="sr-only">lastName</Label>
                   <Input
+                    name="lastName"
                     placeholder="Sobrenome"
                     type="text"
                     autoCapitalize="none"
                     autoCorrect="off"
+                    onChange={handleChange}
+                    value={formData.lastName}
                   />
                 </div>
                 <div className="grid gap-1">
                   <Label className="sr-only">userName</Label>
                   <Input
+                    name="userName"
                     placeholder="Nome de Usuário"
                     autoCapitalize="none"
                     autoCorrect="off"
+                    onChange={handleChange}
+                    value={formData.userName}
                   />
                 </div>{" "}
                 <div className="grid gap-1">
@@ -50,31 +108,45 @@ const RegisterStudents = () => {
                     autoComplete="email"
                     autoCorrect="off"
                     name="email"
+                    onChange={handleChange}
+                    value={formData.email}
                   />
                 </div>
                 <div className="grid gap-1">
                   <div className="flex gap-1">
                     <Label className="sr-only">phoneNumber</Label>
                     <Input
+                      name="phoneNumber"
                       placeholder="Celular"
                       autoCapitalize="none"
                       autoCorrect="off"
+                      onChange={handleChange}
+                      value={formData.phoneNumber}
                     />
                     <Label className="sr-only">dateOfBirth</Label>
                     <Input
+                      name="dateOfBirth"
                       placeholder="Aniversário"
                       autoCapitalize="none"
                       autoCorrect="off"
+                      onChange={handleChange}
+                      value={formData.dateOfBirth}
                     />
                     <Label className="sr-only">document</Label>
                     <Input
                       placeholder="CPF"
                       autoCapitalize="none"
                       autoCorrect="off"
+                      name="document"
+                      onChange={handleChange}
+                      value={formData.document}
                     />
                   </div>
                 </div>
-                <Button className="bg-primaryPurple/50 hover:bg-primaryPurple/80">
+                <Button
+                  type="submit"
+                  className="bg-primaryPurple/50 hover:bg-primaryPurple/80"
+                >
                   Registrar
                 </Button>
               </div>
