ca67cfad274f9201946abd4e2e8b917c2af94066
JonathanAmaral <jhouamaral95@gmail.com>
2024-10-10 18:32:56 -0300

    feat: add form to answer anamnesis by step with progress bar

--- src/pages/Assessment/Anamnesis.tsx
+++ src/pages/Assessment/Anamnesis.tsx
@@ -1,23 +1,35 @@
 import HeaderComponent from "@/components/Header";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { PagedList } from "@/services/Common/Interfaces/PagedList";
-import { Anamnesis as AnamnesisType, getAnamnesisByStudentId } from "@/services/anamnesisService";
+import { Anamnesis as AnamnesisType, getAnamnesisByStudentId, Questionnare } from "@/services/anamnesisService";
 import { Separator } from "@radix-ui/react-separator";
-import { ChevronLeft, MessageSquare } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { CheckCircle, Circle, EllipsisVertical } from "lucide-react"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { useAuth } from "@/contexts/AuthContext";
+import { Button } from "@/components/ui/button";
 
 const Anamnesis: React.FC = () => {
     const { user } = useAuth();
-    
+
     useEffect(() => {
-        getAnamnesisByStudentId(user!.id).then((data) => setData(data));
+        getAnamnesisByStudentId(user!.id, false).then((data) => setData(data));
     }, []);
 
     const [data, setData] = useState<PagedList<AnamnesisType> | undefined>();
+
+    const navigate = useNavigate();
+
+    const goToForm = (anamnesis: AnamnesisType) => {
+        navigate('form', { state: anamnesis });
+    }
+
+    const canAnswered = (anamnesis: AnamnesisType): boolean => {
+        return anamnesis.questionnare.length > 0
+            && anamnesis.questionnare.some(x => x.answer !== null);
+    }
 
     return (
         <div className="container flex flex-col">
@@ -44,25 +56,32 @@ const Anamnesis: React.FC = () => {
                 <TableBody className="font-medium text-white">
                     {data ? data?.items.map(item => {
                         function checkAllIsAnswered(): React.ReactNode {
-                            return item.questionnare.some(x => x.answer !== null)
-                                ? (<CheckCircle color="green" />)
-                                : (<Circle color="red" />);
+                            return canAnswered(item)
+                                ? (<Circle color="red" />)
+                                : (<CheckCircle color="green" />);
                         }
 
                         return (
-                            <TableRow >
+                            <TableRow key={item.id}>
                                 <TableCell>{item.title}</TableCell>
                                 <TableCell>{checkAllIsAnswered()}</TableCell>
                                 <TableCell>
                                     <DropdownMenu>
                                         <DropdownMenuTrigger asChild>
-                                            <EllipsisVertical />
+                                            <Button
+                                              variant="outline">
+                                                <EllipsisVertical />
+                                            </Button>
                                         </DropdownMenuTrigger>
                                         <DropdownMenuContent className="w-56">
                                             <DropdownMenuGroup>
-                                                <DropdownMenuItem >
-                                                    <MessageSquare className="mr-2 h-4 w-4" />
-                                                    <span>Abrir</span>
+                                                <DropdownMenuItem className="text-center" >
+                                                    <Button
+                                                        disabled={!canAnswered(item)}
+                                                        variant="outline"
+                                                        onClick={() => goToForm(item)}>
+                                                        Responder
+                                                    </Button>
                                                 </DropdownMenuItem>
                                             </DropdownMenuGroup>
                                         </DropdownMenuContent>
